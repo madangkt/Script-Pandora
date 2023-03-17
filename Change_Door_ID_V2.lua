28444f21bb94e87250b54f63e465242a68076ae5
madangkt <123939335+madangkt@users.noreply.github.com>
2023-03-17 16:25:01 +0800

    Create Change_Door_ID_V2.lua

---
+++ Change_Door_ID_V2.lua
@@ -0,0 +1,35 @@
+--[ API ]--
+farm = Bot[getBot().name:upper()].farm
+oldDoorID = Bot[getBot().name:upper()].oldDoorID
+newDoorID = Bot[getBot().name:upper()].newDoorID
+webfuck = Bot[getBot().name:upper()].webfuck
+
+--[KM MW? AK JG MW]--
+say("`cSCRIPT MADE By `4Madang.EXE#6264")
+sleep(1000)
+function webfucki(text)
+    local wh = {}
+    wh.url = webfuck
+    wh.username = "M A D S | NIGGA"
+    wh.content = text
+    webhook(wh)
+end
+
+function change(id,newID)
+    for i = 1,#farm do
+        sendPacket(3,"action|join_request\nname|"..farm[i].."\ninvitedWorld|0")
+        sleep(5000)
+            while getTile(math.floor(getBot().x / 32),math.floor(getBot().y / 32)).fg == 6 do
+                sendPacket(3,"action|join_request\nname|"..farm[i].."|"..id.."\ninvitedWorld|0")
+                sleep(2000)
+            end
+        wrench(0,0)
+        sleep(2500)
+        sendPacket(2,"action|dialog_return\ndialog_name|door_edit\ntilex|"..math.floor(getBot().x/32).."|\ntiley|"..math.floor(getBot().y/32).."|\ndoor_name|mm\ndoor_target|\ndoor_id|"..newID.."\ncheckbox_locked|0")
+        sleep(2500)
+        webfucki(" **"..getBot().name:upper().."** IN ||"..getBot().world:upper().."|| CHANGE **"..oldDoorID.."** <:panahpanah:943220544470413372> ||["..newDoorID.."]||")
+    end
+    removeBot(getBot().name)
+end
+
+change(oldDoorID,newDoorID)
