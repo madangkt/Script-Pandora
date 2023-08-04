c7fd188e10d137c3897fd02d6a4ec63ea8612077
madangkt <123939335+madangkt@users.noreply.github.com>
2023-08-04 19:55:56 +0800

    Update Check_Farm_V3.lua

--- Check_Farm_V3.lua
+++ Check_Farm_V3.lua
@@ -1,83 +1,77 @@
 Total_Nuked = 0
 nuked = false
-app = MADS.Aplication:lower()
 
-function Olympus_Multhod()
-    local function warp(world)
-        sendPacket("action|join_request\nname|"..world:upper().."\ninvitedWorld|0",3)
-        sleep(5000)
-    end--
-
-    local function log(text) 
-        file = io.open("WORLD STATUS.txt", "a")
-        file:write(text.."\n")
-        file:close()
-    end
-
-    local function scanFossil()
-        local count = 0
-        for index,fosil in pairs(getTiles()) do
-            if fosil.fg == 3918 then
-                count = count + 1
-            end
+local function warp(world)
+    sendPacket("action|join_request\nname|"..world:upper().."\ninvitedWorld|0",3)
+    sleep(MADS.DelayWarp)
+end--
+local function log(text) 
+    file = io.open("WORLD STATUS.txt", "a")
+    file:write(text.."\n")
+    file:close()
+end
+local function scanFossil()
+    local count = 0
+    for index,fosil in pairs(getTiles()) do
+        if fosil.fg == 3918 then
+            count = count + 1
+            sleep(1)
         end
-        return count
     end
-
-    local function scanReady(id)
-        local count = 0
-        for index,tile in pairs(getTiles()) do
-            if tile.fg == id and tile.ready then
-                count = count + 1
-            end
+    return count
+end
+local function scanReady(id)
+    local count = 0
+    for index,tile in pairs(getTiles()) do
+        if tile.fg == id and tile.ready then
+            count = count + 1
+            sleep(1)
         end
-        return count
     end
-
-    local function UnReady(id)
-        local count = 0
-        for index,tile in pairs(getTiles()) do
-            if tile.fg == id and not tile.ready then
-                count = count + 1
-            end
+    return count
+end
+local function UnReady(id)
+    local count = 0
+    for index,tile in pairs(getTiles()) do
+        if tile.fg == id and not tile.ready then
+            count = count + 1
+            sleep(1)
         end
-        return count
     end
-
-    local function infokan(description)
-        local script = [[
-            $webHookUrl = "]]..MADS.Webhook..[["
-            $content = "]]..description..[["
-            $payload = @{
-                content = $content 
-            }
-            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-            Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
-        ]]
-        local pipe = io.popen("powershell -command -", "w")
-        pipe:write(script)
-        pipe:close()
-    end 
-
-    addHook("onvariant","cekfarm",function(var)
-        if var[0] == "OnConsoleMessage" then
-            if var[1]:find("inaccessible") then
-                nuked = true
-            end
+    return count
+end
+local function infokan(description)
+    local script = [[
+        $webHookUrl = "]]..MADS.Webhook..[["
+        $content = "]]..description..[["
+        $payload = @{
+            content = $content 
+        }
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        Invoke-RestMethod -Uri $webHookUrl -Body ($payload | ConvertTo-Json -Depth 4) -Method Post -ContentType 'application/json'
+    ]]
+    local pipe = io.popen("powershell -command -", "w")
+    pipe:write(script)
+    pipe:close()
+end
+addHook('onvariant','aseli',function(var)
+    if string.find(var[0],'OnConsoleMessage') then
+        if string.find(var[1],'inaccessible') then
+            nuked = true
         end
-    end)
-
-    log("----------------------")
+    end
+end)
+log("-----------------------------------------------")
+while true do
     for i = 1,#MADS.FarmList do
         warp(MADS.FarmList[i])
-        sleep(100)
         if not nuked then
             treek = scanReady(MADS.Tree)
-            sleep(1000)
+            sleep(math.ceil(MADS.DelayAfk / 3))
             treeks = UnReady(MADS.Tree)
-            sleep(1000)
+            sleep(math.ceil(MADS.DelayAfk / 3))
             posil = scanFossil()
-            sleep(1000)
+            sleep(math.ceil(MADS.DelayAfk / 3))
             log(MADS.FarmList[i]:upper().." SAFE | ".."[ "..treek.." Ready & "..treeks.." Not Ready ] | "..posil.." Fossil")
             infokan(MADS.FarmList[i]:upper().." SAFE | ".."[ "..treek.." Ready & "..treeks.." Not Ready ] | "..posil.." Fossil")
             sleep(100)
@@ -89,11 +83,10 @@ function Olympus_Multhod()
             Total_Nuked = Total_Nuked + 1
         end
     end
-    infokan("Total Nuked "..Total_Nuked.." World")
-    log("Total Nuked "..Total_Nuked.." World\n")
-    removeHooks()
-end
-
-if app == "olympus" or app == "multhod" then
-    Olympus_Multhod()
+    if not MADS.Loop then
+        infokan("Total Nuked "..Total_Nuked.." World")
+        log("Total Nuked "..Total_Nuked.." World\n")
+        removeHooks()
+        break
+    end
 end
